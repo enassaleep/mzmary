fdeb014a9c5f581da43f8b054f771979c5890a5b
enassaleep <enassaleep@gmail.com>
2024-08-24 00:58:59 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        mainColor:"#21294B",
+        secontColor:"#797C89"
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+       
+        Expo:["var(--expo-light)"]
+        
+      }
+    },
+  },
+  plugins: [
+    require('tailwindcss-rtl'),
+  ],
+};
